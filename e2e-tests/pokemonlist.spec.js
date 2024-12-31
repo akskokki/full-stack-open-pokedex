b6cb3fa4919fa8758633df73aa6add8efa4445ed
akskokki <akseli.kokki@gmail.com>
2024-12-31 22:21:53 +0200

    Add another test

--- e2e-tests/pokemonlist.spec.js
+++ e2e-tests/pokemonlist.spec.js
@@ -10,4 +10,10 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+  test('pokemon page can be opened', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    expect(page.getByText('overgrow')).toBeDefined()
+    expect(page.getByText('chlorophyll')).toBeDefined()
+  })
 })
