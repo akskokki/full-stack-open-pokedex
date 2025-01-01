19bb4782ac659c0fe365fe2ab5b3724f4cb0cffa
akskokki <akseli.kokki@gmail.com>
2025-01-02 01:53:20 +0200

    Fix intentional bug

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('14') // change this string to ensure a new version deployed
+  res.send('15') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  throw new Error()
   res.send('ok')
 })
 
